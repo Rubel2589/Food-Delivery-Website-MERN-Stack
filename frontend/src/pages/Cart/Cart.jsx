7af2e7d5c9187315dd9b0cf58964431e5e266a18
rubel2589 <rubelali2589@gmail.com>
2025-04-17 20:11:48 +0530

    Oreder Place section added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
+    const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -58,7 +60,7 @@ function Cart() {
               <b>${getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate("/order")}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div className="">
